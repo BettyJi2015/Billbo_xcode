7f4a33f4b299d422d7b9bf3c937df8c42a1b36af
BettyJi2015 <Betty@140.72.208.web-pass.com>
2015-12-13 14:01:46 -0800

    $ git add . # Adds the files in the local repository and stages them for commit. To unstage a file, use 'git reset HEAD YOUR-FILE'.

--- Billbo_xcode/ViewController.swift
+++ Billbo_xcode/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://bettydesign.co/zuoanbeibei/617%20HW/Billbo_parse/");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
